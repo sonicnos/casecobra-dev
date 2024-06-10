eee110846f7c30e61990aae789b1d99a6ff20a8c
sonicnos <107223123+sonicnos@users.noreply.github.com>
2024-06-10 17:14:39 +0200

    "1h30"

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,24 @@
 import React from "react";
-import MaxWithWrapper from "@/components/MaxWithWrapper";
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import Link from "next/link";
 import { buttonVariants } from "./ui/button";
 import { ArrowRight } from "lucide-react";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
-function Navbar() {
-  const user = undefined;
-  const isAdmin = false;
+const Navbar = async () => {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  const isAdmin = user?.email === process.env.ADMIN_EMAIL;
+
   return (
     <nav className="sticky z-[100] h-14 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
-      <MaxWithWrapper>
+      <MaxWidthWrapper>
         <div className="flex h-14 items-center justify-between border-b border-zinc-200">
-          <Link href="#" className="flex z-40 font-semibold">
+          <Link href="/" className="flex z-40 font-semibold">
             case<span className="text-green-600">cobra</span>
           </Link>
+
           <div className="h-full flex items-center space-x-4">
             {user ? (
               <>
@@ -24,17 +29,17 @@ function Navbar() {
                     variant: "ghost",
                   })}
                 >
-                  Sign Out
+                  Sign out
                 </Link>
                 {isAdmin ? (
                   <Link
-                    href="/api/auth/logout"
+                    href="/dashboard"
                     className={buttonVariants({
                       size: "sm",
                       variant: "ghost",
                     })}
                   >
-                    Dashboard
+                    Dashboard ✨
                   </Link>
                 ) : null}
                 <Link
@@ -59,17 +64,19 @@ function Navbar() {
                 >
                   Sign up
                 </Link>
+
                 <Link
-                  href="/configure/login"
+                  href="/api/auth/login"
                   className={buttonVariants({
                     size: "sm",
                     variant: "ghost",
-                    className: "hidden sm:flex items-center gap-1",
                   })}
                 >
                   Login
                 </Link>
+
                 <div className="h-8 w-px bg-zinc-200 hidden sm:block" />
+
                 <Link
                   href="/configure/upload"
                   className={buttonVariants({
@@ -84,9 +91,9 @@ function Navbar() {
             )}
           </div>
         </div>
-      </MaxWithWrapper>
+      </MaxWidthWrapper>
     </nav>
   );
-}
+};
 
 export default Navbar;
